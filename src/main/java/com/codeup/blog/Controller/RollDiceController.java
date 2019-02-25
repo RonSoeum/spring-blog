8e6dec67e6499a1e74635964f2ed5c89a8473521
Ron Soeum <ronsoeum@gmail.com>
2019-02-25 13:55:36 -0600

    Spring Boot: Views - passing data to views

---
+++ src/main/java/com/codeup/blog/Controller/RollDiceController.java
@@ -0,0 +1,36 @@
+package com.codeup.blog.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
+
+        Random rand = new Random();
+        int diceRoll = rand.nextInt(6);
+        diceRoll += 1;
+
+        String result;
+        if (diceRoll == guess) {
+            result = "You guessed the right number!";
+        } else {
+            result = "You are wrong.";
+        }
+
+        model.addAttribute("guess", guess);
+        model.addAttribute("diceRoll", diceRoll);
+        model.addAttribute("result", result);
+        return "roll-dice";
+    }
+
+}// class
