41a7a3d502122b2200263579c51df7f6ab5c546b
Ron Soeum <ronsoeum@gmail.com>
2019-02-28 11:31:08 -0600

    fixed sticky footer

--- src/main/java/com/codeup/blog/Controller/PostController.java
+++ src/main/java/com/codeup/blog/Controller/PostController.java
@@ -66,5 +66,4 @@ public class PostController {
         return "redirect:/posts";
     }
 
-
 }// class
--- src/main/java/com/codeup/blog/Post/Post.java
+++ src/main/java/com/codeup/blog/Post/Post.java
@@ -4,15 +4,13 @@ import javax.persistence.*;
 @Entity
 @Table(name="posts")
 public class Post {
+
     @Id @GeneratedValue
     private long id;
     @Column(nullable = false, length = 100)
     private String title;
     @Column(nullable = false)
     private String body;
-
-    public Post() {
-    }
 
     public Post(long id, String title, String body) {
         this.id = id;
@@ -23,6 +21,9 @@ public class Post {
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
+    }
+
+    public Post() {
     }
 
     public long getId() {
